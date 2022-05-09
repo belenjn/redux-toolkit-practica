6fcfcd110041d882182e96c724ef2fbdb3e35545
Belen <belenjarabanavas31@gmail.com>
2022-05-09 15:53:39 +0200

    add store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { Provider } from "react-redux";
 import { Navbar } from "./components/Navbar";
 import { UserList } from "./components/UserList";
+import store from "./store/index"
 
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
     <Navbar/>
     <UserList/>
-    </>
+    </Provider>
     
   );
 }
